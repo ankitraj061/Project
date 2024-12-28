1a953030a2126f407ad7f1c78e869eae4f12c34f
Ankit Raj <ankitraj07032002@gmail.com>
2024-12-28 10:30:51 +0530

    added

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,55 +1,4 @@
-// import React from 'react';
-// import backV from '../assets/video.mp4';
-// import OurProducts from '../pages/OurProducts';
-// import { Link } from 'react-router-dom';
 
-
-
-// const Hero = () => {
-//   return (
-//     <div className="relative min-h-screen">
-//       {/* Background Video */}
-//       <div className="absolute inset-0 z-0">
-//         <video
-//           className="w-full h-full object-cover"
-//           autoPlay
-//           loop
-//           muted
-//         >
-//           <source
-//             src={backV}
-//             type="video/mp4"
-//           />
-//           Your browser does not support the video tag.
-//         </video>
-//         {/* <div className="absolute inset-0 bg-white/60"></div> Removed backdrop-blur-sm */}
-//       </div>
-      
-//       {/* Content Section */}
-//       <div className="relative z-10 max-w-7xl mx-auto px-4 h-full sm:px-6 lg:px-8 py-32 border border-red-500">
-//         <div className="max-w-2xl border border-red-500 h-full flex flex-col justify-center">
-//           <div className="space-x-4">
-//             <Link to={'/quiz'}>
-//             <button className="bg-emerald-600 text-white px-8 py-3 rounded-full hover:bg-emerald-700 transition-colors">
-//               Know Your Skin
-//             </button>
-
-//             </Link>
-            
-//             <Link to={'/ourproducts'}>
-//               <button className="bg-white text-emerald-600 px-8 py-3 rounded-full border-2 border-emerald-600 hover:bg-emerald-50 transition-colors">
-//                 Shop Our Products
-
-//             </button>
-//             </Link>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Hero;
 import React from 'react';
 import backV from '../assets/video.mp4';
 import { Link } from 'react-router-dom';
@@ -77,7 +26,7 @@ const Hero = () => {
         </p> */}
 
         {/* Buttons */}
-        <div className="space-x-4 flex justify-end py-24 justify-end mt-36">
+        <div className="space-x-4 flex justify-end py-24 justify-end mt-48">
           <Link to={'/quiz'}>
             <button className="bg-emerald-600 text-white px-8 py-3 rounded-full hover:bg-emerald-700 transition-colors">
               Know Your Skin
