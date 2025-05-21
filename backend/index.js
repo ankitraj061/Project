a07ed685e79da995925d233c2a5052796439dca6
Ankit Raj <ankitraj07032002@gmail.com>
2025-05-22 00:28:23 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: 'https://project-72wcq1abz-ankit-rajs-projects-c5d17c57.vercel.app', // your Vercel frontend URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+}));
+
 
 // Create PostgreSQL connection pool
 const pool = new Pool({
